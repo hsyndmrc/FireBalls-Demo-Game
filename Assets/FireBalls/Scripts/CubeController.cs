e4d6fd0b5aad99bf13034e73b3b56231498ca614
hsyndmrc <87519251+hsyndmrc@users.noreply.github.com>
2021-07-19 12:27:32 +0300

    Update CubeController.cs

--- Assets/FireBalls/Scripts/CubeController.cs
+++ Assets/FireBalls/Scripts/CubeController.cs
@@ -1,34 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class CubeController : MonoBehaviour
 {
 
-    [SerializeField] private GameObject cubePrefab1;
-    [SerializeField] private GameObject cubePrefab2;
+    [SerializeField] private GameObject[] cubePrefabs;
+    [SerializeField] private Transform cubeParent;
+    [SerializeField] float translateTime = 0.01f;
 
-
-
-    void Start()
+    public void PreLoadCubes()
     {
-        SimplePool.Preload(cubePrefab1, 50);
-        SimplePool.Preload(cubePrefab2, 50);
-        SpawnLevelCubes();
+        foreach (var item in cubePrefabs)
+            SimplePool.Preload(item, 50);
     }
-
-    public void SpawnLevelCubes()
+    public Transform SpawnLevelCubes(Transform circleTr)
     {
         int rnd = Random.Range(25, 35);
-        Vector3 pos = new Vector3(0, 0.5f, -9.5f);
+        Vector3 pos = circleTr.position;
+        GameObject spawnCube;
+
         for (int i = 0; i < rnd; i++)
         {
-            SimplePool.Spawn(cubePrefab1, pos, cubePrefab1.transform.rotation);
-            pos.y = pos.y + 0.55f;
-            SimplePool.Spawn(cubePrefab2, pos, cubePrefab2.transform.rotation);
-            pos.y = pos.y + 0.55f;
+            foreach (var item in cubePrefabs)
+            {
+                spawnCube = SimplePool.Spawn(item, pos, item.transform.rotation);
+                pos.y -= cubePrefabs[0].transform.localScale.y;
+                spawnCube.transform.parent = cubeParent.transform;
+            }
         }
+        return cubeParent;
     }
+    public void TranslateCubes(int cubesCount)
+    {
+        float height = cubePrefabs[0].transform.localScale.y * cubesCount;
+        float endPointY = cubeParent.localPosition.y + height;
+        float time = cubesCount * translateTime;
+        cubeParent.DOMoveY(endPointY, time);
+    }
+
+
+
+
+
+
 
 
 
